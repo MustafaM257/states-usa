805df98199777939561869e7ea8a31d646ee1c97
mustafa <mustafa.m257@proton.me>
2024-06-13 19:10:30 -0400

    Finalize inital design of navbar

---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { cn } from "@/utils/cn";
+import { IconHome, IconMail, IconInfoOctagon } from "@tabler/icons-react";
+import Link from "next/link";
+const navItems = [
+  { label: "Home", href: "/", icon: IconHome },
+  { label: "About", href: "/about", icon: IconInfoOctagon },
+  { label: "Contact", href: "/contact", icon: IconMail },
+];
+const Header = ({ className }: { className?: string }) => {
+  return (
+    <div
+      className={cn(
+        "fixed md:top-10 inset-x-0 max-w-2xl mx-auto z-50 bg-neutral-900 rounded-full px-4 py-2 text-white shadow-lg",
+        className
+      )}
+    >
+      <nav className="flex justify-between">
+        {navItems.map((item) => (
+          <Link
+            key={item.label}
+            href={item.href}
+            className="flex items-center space-x-2 group"
+          >
+            <item.icon className="shadow-md group-hover:text-red-600" />
+            <span className="text-2xl hidden md:flex">{item.label}</span>
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
