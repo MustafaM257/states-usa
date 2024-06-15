d4434ff1a01036c6e6171c1ef8c7dd9ecf2ae073
mustafa <mustafa.m257@proton.me>
2024-06-15 01:15:45 -0400

    Improve UI of footer and header, perfect consistency

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-gray-200 text-center space-y-4 py-4 px-2">
+    <div className="w-full text-center space-y-4  z-50 bg-white/90 border border-black shadow-2xl md:rounded-full px-4 py-9 text-white ">
       <h1 className="text-blue-700">Made with ❤️ by Mostafa Mohammad </h1>
       <p className="mx-auto mt-3 text-base text-center text-red-600  ">
         This website is a free public resource site, and is not affiliated with
--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header = ({ className }: { className?: string }) => {
   return (
     <div
       className={cn(
-        "fixed md:top-10 inset-x-0 md:max-w-2xl mx-auto z-50 bg-white/90 md:rounded-full px-4 py-2 text-white shadow-lg",
+        "fixed md:top-10 inset-x-0 md:max-w-2xl mx-auto z-50 border border-black bg-white/90 md:rounded-full px-4 py-2 text-white shadow-lg",
         className
       )}
     >
@@ -21,7 +21,7 @@ const Header = ({ className }: { className?: string }) => {
           <Link
             key={item.label}
             href={item.href}
-            className="flex items-center space-x-2 group px-2 py-4"
+            className="flex items-center space-x-2 group px-2 py-3"
           >
             <button className="px-4 py-2 rounded-full border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200">
               <item.icon className="  md:hidden" />
