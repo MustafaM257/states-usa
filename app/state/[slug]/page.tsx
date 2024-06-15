bffb5d3033b24b4289685ecce633211b0562f514
mustafa <mustafa.m257@proton.me>
2024-06-14 22:46:21 -0400

    Initial UI of state finalized

--- app/state/[slug]/page.tsx
+++ app/state/[slug]/page.tsx
@@ -90,62 +90,57 @@ const page = ({ params }: { params: { slug: string } }) => {
               </p>
               <ul role="list" className="mt-8 space-y-8 text-gray-600">
                 <li className="flex gap-x-3">
-                  ICON
                   <span>
                     <strong className="font-semibold text-gray-900">
-                      Push to deploy.
+                      Population:
                     </strong>{" "}
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
+                    {state.population_2020.toLocaleString()}
                   </span>
                 </li>
                 <li className="flex gap-x-3">
-                  {/* <LockClosedIcon
-                    className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                    aria-hidden="true"
-                  /> */}
                   <span>
                     <strong className="font-semibold text-gray-900">
-                      SSL certificates.
+                      Area:
                     </strong>{" "}
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                    qui lorem cupidatat commodo.
+                    {state.total_area_mi2.toLocaleString()} sq mi
                   </span>
                 </li>
                 <li className="flex gap-x-3">
-                  {/* <ServerIcon
-                    className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                    aria-hidden="true"
-                  /> */}
-                  ICON
                   <span>
                     <strong className="font-semibold text-gray-900">
-                      Database backups.
+                      Capital:
                     </strong>{" "}
-                    Ac tincidunt sapien vehicula erat auctor pellentesque
-                    rhoncus. Et magna sit morbi lobortis.
+                    {state.capital}
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <span>
+                    <strong className="font-semibold text-gray-900">
+                      Largest City:
+                    </strong>{" "}
+                    {state.largest_city}
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <span>
+                    <strong className="font-semibold text-gray-900">
+                      Date of Admisson:
+                    </strong>{" "}
+                    {state.ratification_or_admission}
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <span>
+                    <strong className="font-semibold text-gray-900">
+                      Number of Representatives:
+                    </strong>{" "}
+                    {state.representatives}
                   </span>
                 </li>
               </ul>
-              <p className="mt-8">
-                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
-                odio id et. Id blandit molestie auctor fermentum dignissim.
-                Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate
-                et ultrices hac adipiscing egestas. Iaculis convallis ac tempor
-                et ut. Ac lorem vel integer orci.
-              </p>
-              <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-                No server? No problem.
-              </h2>
-              <p className="mt-6">
-                Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam
-                consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod
-                vitae interdum mauris enim, consequat vulputate nibh. Maecenas
-                pellentesque id sed tellus mauris, ultrices mauris. Tincidunt
-                enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam
-                turpis ipsum eu a sed convallis diam.
-              </p>
+              <p className="mt-8"></p>
+              <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900"></h2>
+              <p className="mt-6"></p>
             </div>
           </div>
         </div>
