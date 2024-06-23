604e1ad8b9003f8db751f51e0bef6eef296bd9c2
mustafa <mustafa.m257@proton.me>
2024-06-23 18:22:08 -0400

    Now search by zip code is working

--- app/find/page.tsx
+++ app/find/page.tsx
@@ -1,17 +1,21 @@
 "use client";
-import React from "react";
-import { useState } from "react";
-const page = () => {
-  // const [search, setSearch] = useState("");
+import React, { useState } from "react";
+import findLocation from "@/utils/find";
+const Page = () => {
+  const [search, setSearch] = useState("");
+
   const handleSearch = (e: any) => {
     e.preventDefault();
-    console.log("Searching...");
+    console.log("Searching for:", search);
+    // Add your search logic here
+    findLocation(search);
   };
+
   return (
-    <div className="space-y-10 pt-32 ">
+    <div className="space-y-10 pt-32">
       <div>
         <label
-          htmlFor="name"
+          htmlFor="search"
           className="ml-px block pl-4 text-sm font-medium leading-6 text-gray-900"
         >
           Search for a city, state, or zip code
@@ -19,8 +23,10 @@ const page = () => {
         <form className="mt-2" onSubmit={handleSearch}>
           <input
             type="text"
-            name="name"
-            id="name"
+            name="search"
+            id="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
             className="block w-full rounded-full border-0 px-4 py-1.5 text-blue-600 shadow-sm ring-1 ring-inset ring-red-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6 outline-none"
             placeholder="Jane Smith"
           />
@@ -30,4 +36,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
--- utils/find.ts
+++ utils/find.ts
@@ -1,1 +1,23 @@
-import find from "@/json/find.json";
+import cities from "@/json/cities.json";
+
+// Use a proper function declaration
+const findLocation = (location: string): void => {
+  Object.keys(cities).forEach((key) => {
+    // Now we're in the object containing cities and names
+    // Let's loop through the cities
+    const state_cities = cities[key as keyof typeof cities].cities;
+    // now the state_cities is an object of key value pairs of "city" : array of zip codes
+    Object.keys(state_cities).forEach((city) => {
+      const zip_codes = state_cities[city as keyof typeof state_cities];
+      //   console.log("City:", city, "Zip Codes:", zip_codes);
+      const zip_codes_array = zip_codes as string[];
+      zip_codes_array.forEach((zip) => {
+        if (Number.parseInt(zip) == Number.parseInt(location)) {
+          console.log("Found location:", location, "in", city, key);
+        }
+      });
+    });
+  });
+};
+
+export default findLocation;
